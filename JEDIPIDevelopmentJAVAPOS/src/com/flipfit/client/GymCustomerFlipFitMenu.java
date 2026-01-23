854b8a1c9fdcfb944d044d0d7159a9a9352d22a2
Himanshi <himanshidhiman852@gmail.com>
2026-01-23 14:30:00 +0530

    Added the client files successfully

---
+++ JEDIPIDevelopmentJAVAPOS/src/com/flipfit/client/GymCustomerFlipFitMenu.java
@@ -0,0 +1,9 @@
+package com.flipfit.client;
+import java.util.Scanner;
+
+public class GymCustomerFlipFitMenu {
+    public static void showMenu(Scanner scanner) {
+        System.out.println("\n--- Customer Menu ---");
+        System.out.println("1. View Slots\n2. Book Session\n3. Back");
+    }
+}
---
+++ JEDIPIDevelopmentJAVAPOS/src/com/flipfit/client/GymUserFlipFitMenu.java
@@ -0,0 +1,9 @@
+package com.flipfit.client;
+import java.util.Scanner;
+
+public class GymUserFlipFitMenu {
+    public static void showMenu(Scanner scanner) {
+        System.out.println("\n--- General User Settings ---");
+        System.out.println("1. Change Password\n2. Update Profile\n3. Back");
+    }
+}
